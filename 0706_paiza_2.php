46c4f2fc8b995569fbc51bae9b65b710be5cfed5
Shohei <harulefoot@gmail.com>
2020-12-20 07:49:35 +0900

    ２回目

--- 0706_paiza_2.php
+++ 0706_paiza_2.php
@@ -5,10 +5,11 @@
     $rate=$input_line[1];
     $totalCost=$price;
     $flg=false;
+    // ループをまわす
     while(!$flg){
         // 割引後の値段を更新
         $price = floor($price*(1-($rate*0.01)));
-        //  値段チェック
+        //  値段チェック    
         if($price<1) {
             // １円未満ならループを抜ける
             $flg=true;
